37561960081ccba9d46bdef5f2fdc5d3f4608391
Marcos <mmartinez@uplift.com>
2022-10-13 16:02:23 -0700

    Make pyright use virtualenv or local project python path

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -5,6 +5,27 @@ if not status_ok then
 	return
 end
 
+local path = require('lspconfig/util').path
+
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs({'*', '.*'}) do
+    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+    if match ~= '' then
+      return path.join(path.dirname(match), 'bin', 'python')
+    end
+  end
+
+  -- Fallback to system Python.
+  return exepath('python3') or exepath('python') or 'python'
+end
+
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
@@ -26,6 +47,11 @@ lsp_installer.on_server_ready(function(server)
 	 if server.name == "pyright" then
 	 	local pyright_opts = require("user.lsp.settings.pyright")
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	 	opts = vim.tbl_deep_extend("force", {
+      before_init = function(_, config)
+        config.settings.python.pythonPath = get_python_path(config.root_dir)
+      end
+    }, opts)
 	 end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
