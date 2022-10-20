8de714313b97353d20d122ac39e8a12cfd1eb3fc
Marcos <mmartinez@uplift.com>
2022-10-20 11:52:51 -0700

    Add gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,6 +78,9 @@ return packer.startup(function(use)
   use "nvim-treesitter/playground"
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- snippets
   use "L3MON4D3/LuaSnip"             --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
